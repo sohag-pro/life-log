18550936dd3a10d7bee41dcfb15d2c053dc47c76
Sohag Hasan <shohaghasan575@gmail.com>
2019-12-29 12:03:29 +0600

    Past Added

--- js/init.js
+++ js/init.js
@@ -4,6 +4,8 @@
     $('.sidenav').sidenav();
     $('.timepicker').timepicker();
     $('select').formSelect();
+    $('.datepicker').datepicker();
+    $('.collapsible').collapsible();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
